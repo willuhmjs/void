702c0fb13df4960d9138f79d505d762002295607
William Faircloth <willuhmjs@gmail.com>
2025-04-28 15:45:01 -0400

    log x forwarded proto

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,11 @@
-export { handle } from "./auth"
+import { sequence } from '@sveltejs/kit/hooks';
+import { handle as authenticationHandle } from './auth';
+
+async function headerCheckHandle({ event, resolve }) {
+  // Example: check for a custom header
+  const myHeader = event.request.headers.get('X-Forwarded-Proto');
+  console.log('X-Forwarded-Proto:', myHeader);
+  return resolve(event);
+}
+
+export const handle = sequence(authenticationHandle, headerCheckHandle);
