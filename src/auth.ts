00dc6e44db9d602fa430876fb39e6bfbb915c62e
William Faircloth <willuhmjs@gmail.com>
2025-04-28 17:08:59 -0400

    dont check if consultant on dev lol

--- src/auth.ts
+++ src/auth.ts
@@ -2,12 +2,13 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from './lib/server/prisma/prismaConnection';
 import Keycloak from "@auth/sveltekit/providers/keycloak"
- 
+import { dev } from '$app/environment'; 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   adapter: PrismaAdapter(prisma),
   useSecureCookies: true,
   callbacks: {
     signIn({ profile }) {
+      if (dev) return true;
       return profile.groups.includes("syskids")
     }
   },
