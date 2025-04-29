28863b1a2d86d942bfa9775cf9a5bae7b412a1a5
William Faircloth <willuhmjs@gmail.com>
2025-04-29 15:53:33 -0400

    GET support

--- src/routes/[endpoint]/+server.ts
+++ src/routes/[endpoint]/+server.ts
@@ -54,3 +54,55 @@ export const POST: RequestHandler = async ({ request, params }) => {
         return new Response('Internal Server Error', { status: 500 });
     }
 };
+
+export const GET: RequestHandler = async ({ request, params }) => {
+    const authHeader = request.headers.get('Authorization');
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        console.warn('Unauthorized request: Missing or invalid Authorization header');
+        return new Response('Unauthorized: No Token', { status: 401 });
+    }
+
+    const token = authHeader.substring(7);
+
+    const dbToken = await prisma.token.findFirst({ 
+        where: {
+            token: token
+        },
+        select: {
+            id: true,
+            endpoints: true
+        }
+    });
+    if (!dbToken) {
+        console.warn('Unauthorized request: Token not found in database');
+        return new Response('Unauthorized: Invalid Token', { status: 401 });
+    }
+
+    const hasEndpoint = dbToken.endpoints.find((endpoint) => endpoint.endpoint === `/${params.endpoint}`);
+    if (!hasEndpoint) {
+        console.warn(`Unauthorized request: Token does not have access to endpoint /${params.endpoint}`);
+        return new Response('Unauthorized: Invalid Endpoint', { status: 401 });
+    }
+
+    const headers = new Headers(request.headers);
+    headers.delete('Authorization'); 
+
+    try {
+        console.info(`Proxying GET request to remote endpoint: ${hasEndpoint.remote_endpoint}`);
+        const response = await fetch(hasEndpoint.remote_endpoint, {
+            method: 'GET',
+            headers: headers,
+            redirect: 'follow'
+        });
+
+        console.info(`Response received from remote endpoint with status: ${response.status}`);
+        return new Response(response.body, {
+            status: response.status,
+            headers: response.headers
+        });
+    } catch (error) {
+        console.error('Error proxying GET request:', error);
+        return new Response('Internal Server Error', { status: 500 });
+    }
+};
