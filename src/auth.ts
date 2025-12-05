759fbc6010806ff5ec363780550786a9c076ab1c
ryzenpay <kobe@franssen.xyz>
2025-12-05 15:56:49 -0500

    no group checking

--- src/auth.ts
+++ src/auth.ts
@@ -2,23 +2,15 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from './lib/server/prisma/prismaConnection';
 import Keycloak from "@auth/sveltekit/providers/keycloak"
-import { dev } from '$app/environment'; 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   adapter: PrismaAdapter(prisma),
   useSecureCookies: true,
-  callbacks: {
-    signIn({ profile }) {
-      if (dev) return true;
-      return profile.groups.includes("syskids")
-    }
-  },
   providers: [Keycloak({
     profile(profile) {
         return {
             name: profile.name ?? "",
             email: profile.email ?? "",
             image: profile.image ?? "",
-            groups: profile.groups,
             username: profile.preferred_username ?? "",
         }
     },
