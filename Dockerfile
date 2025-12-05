8444572609d880f5b3cd4464aa52678d935175fb
ryzenpay <kobe@franssen.xyz>
2025-12-05 14:12:19 -0500

    dockerfile fix, logo, readme init, ansible refactor

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,19 @@
 # Use a Node.js base image
 FROM node:18-alpine
 
-# Set the working directory
-WORKDIR /app
-
-# Copy package.json and package-lock.json
-COPY package.json package-lock.json ./
-
-RUN apk add poppler-data poppler-utils
-
-# copy database schema https://github.com/prisma/prisma/discussions/19669#discussioncomment-11884582
-COPY prisma/schema.prisma ./
-
-# Install dependencies
-RUN npm ci
-
-COPY . .
-
-# Build the SvelteKit app
-RUN npm run build
-
-# Expose the port the app runs on
-EXPOSE 3000
-
 ENV NODE_ENV=production
 
-COPY entrypoint.sh ./
-RUN chmod +x entrypoint.sh
+EXPOSE 3000
 
-USER 1000
+USER node
 
+WORKDIR /app
+
+COPY --chown=node:node package*.json ./
+RUN npm ci && npm cache clean --force
+
+COPY --chown=node:node . .
+
+COPY --chown=node:node entrypoint.sh /
 ENTRYPOINT ["./entrypoint.sh"]
-
+CMD ["node", "build/index.js"]
--- ansible/module.py
+++ ansible/module.py
@@ -24,52 +24,61 @@ def main():
     try:
         # Step 1: Check if the token already exists
         print(f"Checking for existing tokens on URL: {api_url}")
-        response = requests.post(
-            f"{api_url}/api/admin",
-            headers=headers,
-            json={"action": "list-tokens"},
-            timeout=10
-        )
-        response.raise_for_status()
-        existing_tokens = {token['name']: token for token in response.json()}
+        existing_tokens = get_tokens(api_url, headers=headers)
         print(f"Existing tokens retrieved: {list(existing_tokens.keys())}")
 
         if name in existing_tokens:
             # Step 2: Update the token's endpoints
             print(f"Updating token '{name}' with new endpoints: {endpoints}")
             token_id = existing_tokens[name]['id']
-            update_response = requests.post(
-                f"{api_url}/api/admin",
-                headers=headers,
-                json={
-                    "action": "update-token-endpoints",
-                    "data": {"id": token_id, "endpoints": endpoints}
-                },
-                timeout=10
-            )
-            update_response.raise_for_status()
+            update_token(api_url,headers, token_id, endpoints)
             print(f"Token '{name}' updated successfully.")
             module.exit_json(changed=True, token=existing_tokens[name]['token'])
         else:
             # Step 3: Create a new token
             print(f"Creating a new token with name '{name}' and endpoints: {endpoints}")
-            create_payload = {
-                "action": "create-token",
-                "data": {"name": name, "token": "", "endpoints": endpoints}
-            }
-            create_response = requests.post(
-                f"{api_url}/api/admin",
-                headers=headers,
-                json=create_payload,
-                timeout=10
-            )
-            create_response.raise_for_status()
-            new_token = create_response.json()
-            print(f"New token created successfully: {new_token}")
-            module.exit_json(changed=True, token=new_token['token'])
+            new_token = create_token(api_url, headers, name, endpoints)
+            print(f"New token created successfully")
+            module.exit_json(changed=True, token=new_token)
     except requests.exceptions.RequestException as e:
         print(f"API request failed: {str(e)}")
         module.fail_json(msg=f"API request failed: {str(e)}")
 
+
+def get_tokens(api_url:str, headers: dict) -> dict:
+    response = requests.post(
+        f"{api_url}/api/admin",
+        headers=headers,
+        json={"action": "list-tokens"},
+        timeout=10
+    )
+    response.raise_for_status()
+    return {token['name']: token for token in response.json()}
+
+def update_token(api_url: str, headers: dict, id, endpoints ):
+    update_response = requests.post(
+        f"{api_url}/api/admin",
+        headers=headers,
+        json={
+            "action": "update-token-endpoints",
+            "data": {"id": id, "endpoints": endpoints}
+        },
+        timeout=10
+    )
+    update_response.raise_for_status()
+
+def create_token(api_url: str, headers: dict, name, endpoints) -> str:
+    create_payload = {
+        "action": "create-token",
+        "data": {"name": name, "token": "", "endpoints": endpoints}
+    }
+    create_response = requests.post(
+        f"{api_url}/api/admin",
+        headers=headers,
+        json=create_payload,
+        timeout=10
+    )
+    create_response.raise_for_status()
+    return create_response.json()['token']
 if __name__ == '__main__':
     main()
